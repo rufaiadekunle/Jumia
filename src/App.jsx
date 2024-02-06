f3913b1c077afb799488e524ca9b8c6942aebef7
Shomzy17 <157720685+Shomzy17@users.noreply.github.com>
2024-02-06 16:35:49 +0100

    navbar 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
 import Top from './components/Top'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
+    <Navbar/>
       <Top/>
     </div>
   )
